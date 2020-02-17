0bf84c2985b7f363ac6a8d8683f562ae12a17fb0
Oleg Elizarov <oleg.elizarov@motley.fi>
2020-02-17 02:28:33 +0200

    Remove check for ym.hit

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -2,8 +2,7 @@ exports.onRouteUpdate = function({ location }, pluginOptions) {
   // Don't track while developing.
   if (
     process.env.NODE_ENV === `production` &&
-    typeof window.ym !== `undefined` &&
-    typeof window.ym.hit === 'function'
+    typeof window.ym !== `undefined`
   ) {
     window.ym(pluginOptions.trackingId, 'hit', (location || {}).pathname);
   }
